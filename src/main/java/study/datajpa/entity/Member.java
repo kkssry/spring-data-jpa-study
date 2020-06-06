171135004a04549c65ea4d52d922d172ddd9e7d4
kkssry <kkssry@gmail.com>
2020-06-06 21:14:19 +0900

    spring data Auditing 사용

--- src/main/java/study/datajpa/entity/Member.java
+++ src/main/java/study/datajpa/entity/Member.java
@@ -14,7 +14,7 @@ import javax.persistence.*;
 )
 
 @NamedEntityGraph(name = "Member.all", attributeNodes = @NamedAttributeNode("team"))
-public class Member extends JpaBaseEntity{
+public class Member extends BaseEntity{
 
     @Id @GeneratedValue
     @Column(name = "member_id")
